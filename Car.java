2c94ef0072b7312360581f0551cdc5e2168e540e
Meysam123 <Meysam.aghlmand@gmail.com>
2019-03-11 08:51:23 -0700

    Adding Constructor

--- Car.java
+++ Car.java
@@ -2,4 +2,11 @@ public class Car {
     private String name;
     private String color;
     private double weight;
+    public Car(){
+        this.name=null;
+    }
+    public Car(String name, int weight){
+        this.name=name;
+        this.weight=weight;
+    }
 }
